1429c43a3bad425af8aeabde17b6f9bb2df52b13
Kris Shanks <shanks@sonic.net>
2014-04-21 11:02:11 -0700

    release 1
    pseudocode

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,14 +4,18 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? input is an array
+# What is the output? (i.e. What should the code return?) for total the output is a number that is the sum of all the numbers in the array
+# for sentence_maker the output is  string with the first letter capitalized and a period at the end
 # What are the steps needed to solve the problem?
-
+# total method - set a variable equal to 0 that will accumulate the total. Add each element of the array to the accumulator
+# sentence_maker - set a variable equal to the first element in the array, and capitalize it.
+# then starting with the second element of the array, join each element to the capitalized string.
+# return the resulting string with a period added to the end.
 
 # 2. Initial Solution
 
