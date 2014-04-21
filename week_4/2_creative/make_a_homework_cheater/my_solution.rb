53c24fb879d88b77dbe8c73a36698bb6d0300f8a
Kris Shanks <shanks@sonic.net>
2014-04-21 15:02:36 -0700

    release 1
    driver code

--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -1,7 +1,12 @@
 # U2.W4: Homework Cheater!
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
+# 1. Template
+# template = "NAME lived in DATE. HE/SHE was an important person. THESIS. I have learned a lot about NAME 's contribution."
+# topic1 = "George Washington lived in 1776. He was an important person. He chopped down a cherry tree, but did not lie. I have learned a lot about George Washington's contribution."
+# topic2 = "Harrison Ford lived in 1976. He was an important person. He is my favorite movie actor. I have learned a lot about Harrison Ford's contribution."
+# topic3 = "Marie Curie lived in 1903. She was an important person. She discovered radioactivity. I have learned a lot about Marie Curie's contribution."
 
 # 2. Pseudocode
 
