44e2d65812ecb8ab23b5c23a2218deeaedd4436e
Kris Shanks <shanks@sonic.net>
2014-04-21 16:29:29 -0700

    release 1
    pseudocode

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -7,9 +7,15 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? a number
+# What is the output? (i.e. What should the code return?) a string with commas separating every three numbers
 # What are the steps needed to solve the problem?
+# convert the number to a string
+# loop until the counter == the string.length
+# every time the counter is divisible by 4, add a comma to the front of the array.
+# starting from the back of the string, add each character of the string to the front of an array.
+# join the array into a string and return it
+
 
 
 # 2. Initial Solution
