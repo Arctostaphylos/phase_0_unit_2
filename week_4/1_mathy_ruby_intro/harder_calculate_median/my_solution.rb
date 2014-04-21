3286a8a40e49a17396659ef2788c2f59982d1272
Kris Shanks <shanks@sonic.net>
2014-04-21 12:03:41 -0700

    release 1
    pseudocode

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -4,13 +4,16 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? an array
+# What is the output? (i.e. What should the code return?) either a number or string that is the median value
 # What are the steps needed to solve the problem?
+# sort the array
+# if the array is even - take the average of the middle two numbers which will be at position array.length/2 and array.length/2 - 1
+# if the array is odd, return the middle number at position array.length/2
 
 
 # 2. Initial Solution
