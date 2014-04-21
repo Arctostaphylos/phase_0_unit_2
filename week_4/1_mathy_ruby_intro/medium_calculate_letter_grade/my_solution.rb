261d800f22e96c29e6735efb65273f9ca195808c
Kris Shanks <shanks@sonic.net>
2014-04-21 11:27:14 -0700

    release 1
    pseudocode

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -4,13 +4,16 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? an array
+# What is the output? (i.e. What should the code return?) a string with the letter grade
 # What are the steps needed to solve the problem?
+# set a variable called average equal to the sum of the array divided by the array.length
+# set up a case statement so that the letter grade is returned for each range of the average
+
 
 
 # 2. Initial Solution
