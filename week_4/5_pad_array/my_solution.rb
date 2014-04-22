b8b5e67fa3cfd6c3b3519a3b903d46b071902f73
Kris Shanks <shanks@sonic.net>
2014-04-21 17:21:31 -0700

    release 1
    pseudocode

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -4,14 +4,19 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? an array, pad parameter and optional value for the array.
+# What is the output? (i.e. What should the code return?) either a new array 
+# or a modified array
 # What are the steps needed to solve the problem?
-
+# determine array length
+# if the pad value is less than or equal to the array length, return the original array
+# find the difference between the pad parameter and the array length
+# initialize a new array using the difference and optional value
+# add the two arrays together
 
 # 2. Initial Solution
 
