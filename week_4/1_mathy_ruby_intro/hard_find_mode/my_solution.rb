093e03decb06d9e876b8000fa17d3f04f335ce0a
Kris Shanks <shanks@sonic.net>
2014-04-23 08:05:21 -0700

    release 1
    pseudocode

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -4,14 +4,20 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? an array
+# What is the output? (i.e. What should the code return?) an array with the 
+#most common values from the original array
 # What are the steps needed to solve the problem?
-
+#initialize a hash with a default value of 0
+# iterate over the array and add the array element as a key to the hash with a 
+# value of the hash value for that key + 1
+# set up an answer array to collect the most common array elements
+# iterate over the hash and add the key to the answer array if the value is values.max
+# return the answer array
 
 # 2. Initial Solution
 
