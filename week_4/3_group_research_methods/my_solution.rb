05789b78aba625805758227407dda03045842de1
Kris Shanks <shanks@sonic.net>
2014-04-23 09:27:51 -0700

    person 1 solution
    using the method #select

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -6,11 +6,11 @@ my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4,
 
 # Person 1's solution
 def my_array_finding_method(source, thing_to_find)
-  # Your code here!
+  source.select{|x| x.to_s.include?(thing_to_find)}
 end
 
 def my_hash_finding_method(source, thing_to_find)
-  # Your code here!
+  source.select{|name, age| age == thing_to_find}.keys
 end
 
 # Identify and describe the ruby method you implemented. 
@@ -78,8 +78,10 @@ p my_array_deletion_method(i_want_pets, "a") == ["I", 4, "pets", "but", "I", "on
 p my_hash_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie" => 5, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
 
 # Reflect!
-# 
-# 
-# 
-# 
-# 
+# select on an array returns a new array of elements for which the code
+# block returns true. In this case I was testing whether each element in
+# the source array included a particular letter.
+# select on a hash returns a hash of the key/value pairs for which the
+# code block is true. In this case, I was testing whether the age matched 
+# the value 3. In order to return an array of the pet names, I called #keys
+# on the resulting hash.
