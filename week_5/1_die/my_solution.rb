08bb531ae33694209cbe637232e22b04970e8331
Kris Shanks <shanks@sonic.net>
2014-04-27 08:34:36 -0700

    solution

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,28 +1,31 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: sides
+# Output: for #sides the number of sides, for #roll a random number between 1 and sides.
+# Steps: raise an argument if sides < 1, set the instance variable to sides.
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+    if sides < 1
+    	raise ArgumentError.new ("sides must be greater than 1")
+    end
+    @sides = sides
   end
   
   def sides
-    # code goes here
+    return @sides
   end
   
   def roll
-    # code goes here
+    return (rand(sides))+1
   end
 end
 
@@ -37,6 +40,9 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+p die = Die.new(6)
+p die.sides == 6
+p die.roll >=1 && die.roll <= 6
 
 
 
