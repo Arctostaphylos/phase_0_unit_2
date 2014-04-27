008d4649c5ae9470764b526fe0b7d48382c4c9b4
Kris Shanks <shanks@sonic.net>
2014-04-27 08:47:02 -0700

    pseudocode

--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -1,13 +1,16 @@
 # U2.W5: Die Class 2: Arbitrary Symbols
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: an array of strings
+# Output: a randomly selected string from the array 
+# Steps: for initialize - set up two variables, one is the array, the other is the array.length
+# sides will return array.length
+# roll will calculate a random number between 0 and array.length, and use that as an index
+# to return one of the strings from the array.
 
 
 # 3. Initial Solution
