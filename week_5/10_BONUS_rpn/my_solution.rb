ca28d0ba65be00c47ea3e81715284fe5d41139f7
Kris Shanks <shanks@sonic.net>
2014-04-28 12:48:06 -0700

    solution

--- week_5/10_BONUS_rpn/my_solution.rb
+++ week_5/10_BONUS_rpn/my_solution.rb
@@ -1,35 +1,90 @@
 # U2.W5: Implement a Reverse Polish Notation Calculator
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: a string with numbers and operators	
+# Output: a number
 # Steps:
+# convert the string to an array
+# iterate through the array
+# if the element is a number, push it to answer_array
+# if the element is an operator, pop the last two elements from answer_array, perform the
+# operation, and push the result back to answer_array
+# answer is the remaining number in the answer_array
 
 
 # 3. Initial Solution
 
-class RPNCalculator
-  # Define your methods, here!
-end
+#class RPNCalculator
+ # def evaluate(expression)
+  #	expression_array = expression.split(" ")
+  #	answer_array = []
+  #	expression_array.each do |x|
+  #		if x =~ /^[-+]?[0-9]*$/
+  #			answer_array << x.to_i
+  #		else
+  #			second_num = answer_array.pop
+  #			first_num = answer_array.pop		
+  #			if x == "*"
+  #				answer_array << (first_num * second_num)
+  #			elsif x == "+"
+  #				answer_array << (first_num + second_num)
+  #			else x == "-"	
+  #				answer_array << (first_num - second_num)
+  #			end
+  #						
+	#	end  
+#	end
+#
+ # end
+ 
+#end
 
 
 
 # 4. Refactored Solution
 
+class RPNCalculator
+  def evaluate(expression)
+  	expression_array = expression.split(" ")
+  	answer_array = []
+  	expression_array.each do |x|
+  		if x =~ /[0-9]+/ # returns true if x contains one or more digits
+  			answer_array << x.to_i
+  		else
+  			second_num = answer_array.pop
+  			first_num = answer_array.pop		
+  			if x == "*"
+  				answer_array << (first_num * second_num)
+  			elsif x == "+"
+  				answer_array << (first_num + second_num)
+  			else x == "-"	
+  				answer_array << (first_num - second_num)
+  			end
+  						
+		end  
+	
+	end
+	return answer_array[0]
+  end
+
+ 
+end
 
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-
-
-
-
+calc = RPNCalculator.new
+p calc.evaluate('1 2 +') == 3
+p calc.evaluate('2 5 *') == 10
+p calc.evaluate('50 20 -') == 30
+p calc.evaluate('1 2 3 4 + + +') == 10
 
 
 # 5. Reflection 
+# 
